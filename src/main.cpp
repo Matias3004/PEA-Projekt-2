737a83f371f4e795010e0fa64b335abe974d15a7
Mateusz Muzioł <matias3004@gmail.com>
2022-11-23 00:09:15 +0100

    main method

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "../inc/Graph.hh"
+#include "../inc/TS.hh"
+#include "../inc/MainMenu.hh"
+#include <time.h>
+
+int main()
+{
+    MainMenu mainMenu;
+    srand(time(NULL));
+    mainMenu.enable();
+}
